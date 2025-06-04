d30d60176755b6c9fcceee15d6f56b9ee5f337aa
coosoti <charlesosoti@gmail.com>
2025-06-04 14:05:16 +0300

    [Feature] - Create models and serializers for products service

---
+++ product_service/products/serializers.py
@@ -0,0 +1,18 @@
+# Import the serializers module from Django REST Framework
+from rest_framework import serializers
+
+# Import the Product model from the current app's models.py
+from .models import Product
+
+# Define a serializer class for the Product model
+# A serializer converts model instances to JSON and validates input data
+class ProductSerializer(serializers.ModelSerializer):
+
+    # Meta class is used to provide metadata to the serializer
+    class Meta:
+        # Specifies the model that the serializer is based on
+        model = Product
+
+        # Use '__all__' to automatically include all fields from the Product model
+        # You can also list fields explicitly like: ['id', 'name', 'description', 'price', 'stock']
+        fields = '__all__'
